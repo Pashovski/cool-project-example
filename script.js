5a0b5b0b613b0fd7ea6412902f9950ed1bc5d5b6
bricejokung <bjokung@gmail.com>
2021-09-14 12:00:39 -0400

    added some javascripts

--- script.js
+++ script.js
@@ -1,7 +1,19 @@
-console.log("connected")
-console.log("connected")
-console.log("connected")
-console.log("connected")
-console.log("connected")
-console.log("connected")
-console.log("connected")
+const BASE_URL = "http://localhost:3000/lexus"
+
+// when the page loads create an image element and place it inside the div with the lexus-catalogue
+
+
+// Make a Get request for Lexus Data
+
+ 
+fetch (BASE_URL)
+    .then(function(res){
+        return res.json()
+    })
+.then(function(lexusData){
+// For every peace of data we will render it
+    lexusData.forEach(renderLexus)
+
+// for each take each element in the array and passes it to our call back function
+
+})
